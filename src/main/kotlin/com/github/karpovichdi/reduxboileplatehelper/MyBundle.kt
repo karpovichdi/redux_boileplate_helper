306cb2089670c4cbe5004a97b3ca02de59823ef0
GitHub Action <action@github.com>
2022-01-18 13:37:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/karpovichdi/reduxboileplatehelper/MyBundle.kt
+++ src/main/kotlin/com/github/karpovichdi/reduxboileplatehelper/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.karpovichdi.reduxboileplatehelper
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/karpovichdi/reduxboileplatehelper/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.karpovichdi.reduxboileplatehelper.services
+
+import com.github.karpovichdi.reduxboileplatehelper.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
